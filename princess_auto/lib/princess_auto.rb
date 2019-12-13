2fcc92b2120c6fdf18323526a91bf2af5851b2a4
darkphoenix662 <tree662@protonmail.com>
2019-12-13 16:54:04 -0700

    set up basic file structure

---
+++ princess_auto/lib/princess_auto.rb
@@ -0,0 +1,6 @@
+require "princess_auto/version"
+
+module PrincessAuto
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ princess_auto/lib/princess_auto/version.rb
@@ -0,0 +1,3 @@
+module PrincessAuto
+  VERSION = "0.1.0"
+end
