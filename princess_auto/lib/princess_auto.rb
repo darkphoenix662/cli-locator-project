4c5862baa2493d7311196dd72b5387de0e3fd96b
darkphoenix662 <tree662@protonmail.com>
2019-12-15 17:33:02 -0700

    Initialize project

--- princess_auto/lib/princess_auto.rb
+++ princess_auto/lib/princess_auto.rb
@@ -1,6 +1,33 @@
 require "princess_auto/version"
 
 module PrincessAuto
-  class Error < StandardError; end
-  # Your code goes here...
+  class Locator
+
+    attr_writer :list_stores
+    attr_reader :greeting, :location
+      #postal code and location are the objects that make this CLI click
+
+    @all = []
+
+    def initialize(greeting)
+      @greeting = greeting
+      @list_stores = []
+    end
+
+    def greeting
+      puts "Welcome to Princess Auto. Please enter your postal code so we can direct you to nearest store."
+    end
+    
+    def list_stores
+      if
+        puts "Here is the store closest to you!"
+      else
+        puts "Does not compute. Please enter a valid postal code."
+      end
+    end
+
+    def ending
+      puts "Thank you for choosing Princess Auto for your shopping needs!"
+    end
+  end
 end
